9e1eaad56ca35e14288dd6833203123767677482
Altukhov Artem <altukhov.artem.dev@gmail.com>
2018-10-19 18:34:42 +0300

    third commit

--- Timer.cs
+++ Timer.cs
@@ -12,20 +12,19 @@ namespace Memory.Timers
         private DateTime _startTime;
         public long Elapsed;
         public static string report;
-        public static int _level = 0;
+        public static int _level;
 
         public Timer()
         {
-            _id = string.Empty;
+            _id = "*";
             _startTime = DateTime.Now;
-            _level++;
         }
 
         public Timer(string timerID)
         {
             _id = timerID;
             _startTime = DateTime.Now;
-            _level++;
+
         }
 
         public static IDisposable Start() => new Timer();
@@ -35,7 +34,7 @@ namespace Memory.Timers
 
         public void Dispose()
         {
-            Elapsed = (DateTime.Now - _startTime).Milliseconds;
+            Elapsed = (DateTime.Now - _startTime).Seconds;
             
             report += new string(' ', 4 * _level) + _id + new string(' ', 20 - 4 * _level - _id.Length) + ": " + Elapsed + '\n';
         }
