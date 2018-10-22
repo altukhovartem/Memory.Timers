ccbc054f651804ebda6ad92130e9d941acd703c5
Altukhov Artem <altukhov.artem.dev@gmail.com>
2018-10-22 19:37:53 +0300

    fourth commit

--- Timer.cs
+++ Timer.cs
@@ -8,38 +8,44 @@ namespace Memory.Timers
 { 
     public class Timer : IDisposable
     {
-        private string _id;
+        [field: ThreadStatic]
+        public static string Report { get; private set; }
+        private static int _level = -1;
+
+        [ThreadStatic]
+        private static Timer _current;
+        private Timer _parent;
+        
+        private string _id = "*";
         private DateTime _startTime;
-        public long Elapsed;
-        public static string report;
-        public static int _level;
+        private DateTime _stopTime;
+        public int Elapsed;
+
 
         public Timer()
         {
-            _id = "*";
             _startTime = DateTime.Now;
+            ++_level;
+
+            _parent = _current;
+            _current = this;
         }
 
         public Timer(string timerID)
+            :this()
         {
             _id = timerID;
-            _startTime = DateTime.Now;
-
         }
 
         public static IDisposable Start() => new Timer();
         public static IDisposable Start(string timerID) => new Timer(timerID);
 
-        public static string Report => report;
-
         public void Dispose()
         {
-            Elapsed = (DateTime.Now - _startTime).Seconds;
-            
-            report += new string(' ', 4 * _level) + _id + new string(' ', 20 - 4 * _level - _id.Length) + ": " + Elapsed + '\n';
+            _stopTime = DateTime.Now;
+            Elapsed = (int)(Math.Round(Decimal.Divide((_stopTime - _startTime).Milliseconds, 100)) * 100);
+            Report += new string(' ', 4 * _level) + _id + new string(' ', 20 - 4 * _level - _id.Length) + ": " + Elapsed + '\n';
+            --_level;
         }
     }
 }
-
-
-//report += new string(' ', 4*_level) + _id + new string(' ', 20 - 4*_level - _id.Length) + ": " + Elapsed + '\n';
