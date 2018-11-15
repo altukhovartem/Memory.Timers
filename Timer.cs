8dd86ac7573d71f87c26e21092a2ab9f7e736261
aaltukhov <altukhov.artem.dev@gmail.com>
2018-11-15 20:36:35 +0300

    sixth commit

--- Timer.cs
+++ Timer.cs
@@ -19,42 +19,43 @@ namespace Memory.Timers
                 return _report; 
             }
         }
-
-        private static Dictionary<string, string> timerDictionary = new Dictionary<string, string>();
-
+        
         [ThreadStatic]
         private Timer _parent;
         private static Timer _current;
+
         public int _level = 0;
-        
-        private string _id = "*";
+        private string _id = string.Empty;
         private DateTime _startTime;
         public int Elapsed;
         private static bool _printed;
 
+        public static Queue<string> _queueOfReports = new Queue<string>();
+
         public Timer()
         {
             if(_printed) ClearPrintedReport();
-            
             _startTime = DateTime.Now;
+            _level = (_parent == null) ? 0 : _parent._level + 1;
+            _id = "*";
             
             CreateReferences();
-
-            timerDictionary.Add(_id, string.Empty);
-
-            _level = (_parent == null) ? 0 : _parent._level + 1;
         }
 
         public Timer(string timerID)
-          : this()
         {
+            if (_printed) ClearPrintedReport();
+            CreateReferences();
+
+            _startTime = DateTime.Now;
+            _level = (_parent == null) ? 0 : _parent._level + 1;
             _id = timerID;
         }
 
         public void Dispose()
         {
             Elapsed = (int)(Math.Round(Decimal.Divide((DateTime.Now - _startTime).Milliseconds, 100)) * 100);
-            _report += new string(' ', 4 * _level) + _id + new string(' ', 20 - 4 * _level - _id.Length) + ": " + Elapsed + '\n';
+            _queueOfReports.Enqueue(new string(' ', 4 * _level) + _id + new string(' ', 20 - 4 * _level - _id.Length) + ": " + Elapsed + '\n');
             RemovesReferenses();
 
             _level = (_current == null) ? 0 : this._level - 1;
@@ -67,14 +68,13 @@ namespace Memory.Timers
         {
             _parent = _current;
             _current = this;
-
         }
 
         private void ClearPrintedReport()
         {
             _printed = false;
             _report = string.Empty;
-            timerDictionary.Clear();
+            _queueOfReports.Clear();
         }
 
         private void RemovesReferenses()
